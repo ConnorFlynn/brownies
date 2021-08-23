43594359133cd41c447beb586b98de13f00e07f0
ConnorFlynn <connor.flynn@student.chaminade.edu>
2021-08-23 15:33:47 -0700

    csv read in

---
+++ desserts_match_cf.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(rvest)
+
+
+webpage <- read_html("https://www.eatthis.com/iconic-desserts-united-states/")
+
+dessert_elements<- html_elements(webpage, "h2")
+dessert_listing <- dessert_elements %>%
+  html_text2() %>% # extracting the text associated with this type of elements of the webpage
+  as_tibble() %>% # make it a data frame
+  rename(dessert = value) %>% # better name for the column
+  head(.,-3) %>% # 3 last ones were not desserts
+  rowid_to_column("rank") %>% # adding a column using the row number as proxy for the rank
+  write_csv("iconic_desserts.csv") # save it as csv
